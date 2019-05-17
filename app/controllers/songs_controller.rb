2b42712c3b452c85d9c2dbe48767f973c678a63e
mmorales4050 <mmorales4050@gmail.com>
2019-05-17 14:09:35 -0400

    added files

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,28 @@
 class SongsController < ApplicationController
+
+  def create
+    song = Song.create(song_params)
+    song.file.attach(song_params[:file])
+  end
+
+  def show
+    song = Song.find_by(id: params[:id])
+
+    if song.file.attached?
+      render json: {song: song, audio: rails_blob_url(song.file)}
+    end
+  end
+
+  def index
+    Song.all.each do |song|
+      render json: {song: song, audio: rails_blob_url(song.file)}
+    end
+  end
+
+
+  private
+
+  def song_params
+    params.permit(:name, :artist, :file)
+  end
 end
