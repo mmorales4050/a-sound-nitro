0ac7d5f3a8f906217d97fa3affe73ae8bd3675d7
mmorales4050 <mmorales4050@gmail.com>
2019-05-26 16:28:36 -0400

    update

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,11 +3,19 @@ class SongsController < ApplicationController
   # has_many :playlists, through: :playlist_songs
 
   def create
+    songs = []
     song_number = (params.keys.length - 2)/3
 
     song_number.times { |index|
-      song = Song.create(name: params[("title" + index.to_s).to_sym], artist: params[("artist" + index.to_s).to_sym], index: params[("index" + index.to_s).to_sym]).file.attach(params[("file" + index.to_s).to_sym])
+      song = Song.create(name: params[("title" + index.to_s).to_sym], artist: params[("artist" + index.to_s).to_sym], index: params[("index" + index.to_s).to_sym])
+
+      song.file.attach(params[("file" + index.to_s).to_sym])
+
+      song_copy = {id: song.id, name: song.name, artist: song.artist, url: rails_blob_url(song.file), index: song.index}
+
+      songs.push(song_copy)
     }
+    render json: songs
   end
 
   def show
@@ -19,9 +27,13 @@ class SongsController < ApplicationController
   end
 
   def song_object
-    Song.all.map do |song|
-      {id: song.id, index: song.index, name: song.name, artist: song.artist, url: rails_blob_url(song.file)}
+    songs = []
+    Song.all.each do |song|
+      if song.file.attached?
+        songs << {id: song.id, index: song.index, name: song.name, artist: song.artist, url: rails_blob_url(song.file)}
+      end
     end
+    songs
   end
 
   def index
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,6 @@ class Song < ApplicationRecord
   has_one_attached :file
   has_many :playlist_songs
   has_many :playlists, through: :playlist_songs
+
+  attr_accessor :url
 end
