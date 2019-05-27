eaf129b24af4f3160110114d5d352ef9449e8a72
mmorales4050 <mmorales4050@gmail.com>
2019-05-27 02:01:06 -0400

    added playlists

--- app/controllers/playlist_songs_controller.rb
+++ app/controllers/playlist_songs_controller.rb
@@ -2,6 +2,12 @@ class PlaylistSongsController < ApplicationController
 
   def create
     PlaylistSong.create(song_id: params[:songId], playlist_id: params[:playlistId])
+    playlist = Playlist.all.find(params[:playlistId])
+    render json: {id: playlist.id, name: playlist.name, songs: playlist.songs.map do |song|
+      {name: song.name, artist: song.artist, url: rails_blob_url(song.file)}
+    end
+  }
+
   end
 
   def index
--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,7 +2,8 @@ class PlaylistsController < ApplicationController
 
   def create
     playlist_name = params[:name]
-    Playlist.create(name: playlist_name, user_id: 1)
+    playlist = Playlist.create(name: playlist_name, user_id: 1)
+    render json: playlist
   end
 
   def index
@@ -14,7 +15,8 @@ class PlaylistsController < ApplicationController
     user.playlists.map do |playlist|
       {id: playlist.id, name: playlist.name, songs: playlist.songs.map do |song|
         {name: song.name, artist: song.artist, url: rails_blob_url(song.file)}
-      end}
+      end
+    }
     end
   end
 
